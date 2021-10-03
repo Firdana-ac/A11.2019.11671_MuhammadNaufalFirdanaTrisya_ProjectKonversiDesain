f4f23aa4b24517e7a69af506da93e6e05b086793
unknown <you@example.com>
2021-10-03 15:17:51 +0700

    Bagian 2

---
+++ dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/WelcomeSlideSuperhero_11671.java
@@ -0,0 +1,13 @@
+package polinema.ac.id.dtschapter03_starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeSlideSuperhero_11671 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_slide_superhero_11671);
+    }
+}
