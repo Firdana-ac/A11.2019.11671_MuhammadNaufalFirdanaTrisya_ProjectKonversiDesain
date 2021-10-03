2143158b845117db1eafe17633a85ed74d8f7838
unknown <you@example.com>
2021-10-03 15:33:38 +0700

    Bagian 3

---
+++ dtschapter03-starter-master/app/src/main/java/polinema/ac/id/dtschapter03_starter/ForgotPassword_11671.java
@@ -0,0 +1,13 @@
+package polinema.ac.id.dtschapter03_starter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForgotPassword_11671 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_11671);
+    }
+}
